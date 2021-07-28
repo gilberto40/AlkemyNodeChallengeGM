87396bccc89383556eb6dc076ae1af26c493a56c
GILBERTO JOSE MATERANO PICHARDO <materano.gj@gmail.com>
2021-07-28 00:15:38 -0300

    models has been created

---
+++ src/business/models/Transaction.js
@@ -0,0 +1,9 @@
+function Transaction(desc, amount, date, type) {    
+    this.desc = desc;
+    this.amount = amount;
+    this.date = date;
+    this.type = type;
+    this.category = category;
+}
+
+export default Transaction;
